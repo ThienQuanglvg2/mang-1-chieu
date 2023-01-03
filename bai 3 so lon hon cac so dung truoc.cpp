e5376316ec3cd64a4a35d89053cb8f91375b9da6
ThienQuanglvg2 <106306026+ThienQuanglvg2@users.noreply.github.com>
2023-01-03 08:49:00 +0700

    Add files via upload

---
+++ bai 3 so lon hon cac so dung truoc.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+main(){
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+int Max1=-1e9-1, Max2=-1e9-1;
+for(int i=0;i<n;i++){
+    if(a[i]>Max1){
+        cout<<a[i]<<" ";
+    }
+    if(a[i]>Max1){
+        Max1=a[i];
+    }
+    }
+
+}
